8a4a906c427aeccbdd994c62e50e463a1fdeaecf
mvansan <anmac2206@gmail.com>
2024-11-18 15:21:11 +0700

    register/login function

--- server.c
+++ server.c
@@ -4,18 +4,46 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "init_db.c"
+#include "auth.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
+void handle_client_request(int client_socket) {
+    char buffer[BUFFER_SIZE] = {0};
+    read(client_socket, buffer, BUFFER_SIZE);
+
+    char command[10], username[50], password[50];
+    sscanf(buffer, "%s %s %s", command, username, password);
+
+    if (strcmp(command, "REGISTER") == 0) {
+        printf("Registering user: %s\n", username);
+        int result = register_user(username, password);
+        if (result == 0) {
+            send(client_socket, "Registration successful", strlen("Registration successful"), 0);
+        } else {
+            send(client_socket, "Registration failed", strlen("Registration failed"), 0);
+        }
+    } else if (strcmp(command, "LOGIN") == 0) {
+        printf("Logging in user: %s\n", username);
+        int result = login_user(username, password);
+        if (result == 1) {
+            send(client_socket, "Login successful", strlen("Login successful"), 0);
+        } else {
+            send(client_socket, "Login failed", strlen("Login failed"), 0);
+        }
+    } else {
+        send(client_socket, "Unknown command", strlen("Unknown command"), 0);
+    }
+}
+
 int main() {
     int server_fd, new_socket;
-    init_database();
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE] = {0};
-    char *welcome_message = "Welcome to the Exam Server!";
+
+    init_database();
 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Socket failed");
@@ -50,16 +78,20 @@ int main() {
 
     printf("Server is listening on port %d\n", PORT);
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("Accept failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("Accept failed");
+            close(server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Accepted a new client connection\n");
+
+        handle_client_request(new_socket);
+
+        close(new_socket);
     }
 
-    send(new_socket, welcome_message, strlen(welcome_message), 0);
-    printf("Welcome message sent to client\n");
-
-    close(new_socket);
     close(server_fd);
     return 0;
 }

2307d448c2f71004cace31c14ce8fa376b8d1f0a
lqCi-sun <lqci.cql@gmail.com>
2024-12-02 12:02:17 +0700

    creat_room

--- init_db.c
+++ init_db.c
@@ -18,14 +18,29 @@ void init_database() {
                       "username TEXT NOT NULL UNIQUE, "
                       "password TEXT NOT NULL);";
 
+    const char *exam_rooms = "CREATE TABLE IF NOT EXISTS exam_rooms ("
+                            "room_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                            "name TEXT NOT NULL, "
+                            "num_questions INTEGER NOT NULL, "
+                            "time_limit INTEGER NOT NULL, "
+                            "status TEXT NOT NULL DEFAULT 'not_started');";
     rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
-
     if (rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", err_msg);
         sqlite3_free(err_msg);
-    } else {
-        printf("Database initialized successfully\n");
+        sqlite3_close(db);
+        return;
     }
+
+    rc = sqlite3_exec(db, exam_rooms, 0, 0, &err_msg);
+    if (rc != SQLITE_OK) {
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+        sqlite3_close(db);
+        return;
+    }
+
+    printf("Database initialized successfully\n");
 
     sqlite3_close(db);
 }
